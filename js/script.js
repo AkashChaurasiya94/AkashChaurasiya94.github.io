1defff37dc40e61829df057fe1781ea2165b0d49
Akash Chaurasiya <AkashChauarasiya94@gmail.com>
2022-08-19 01:12:36 +0530

    add portfolio code

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/*====================================== typing animation =================================*/
+var typed = new Typed(".typing",{
+    strings:["","Web Developer","Mechanical Engineer","Web Designer"],
+    typeSpeed:80,
+    BackSpeed:60,
+    loop:true
+})
+
+// const btnEl = document.querySelector('btn1');
+// const toggleOptions = () => {
+//     const wrapperEl = document.querySelector('wrapper');
+//     const iconEl = btnEl.querySelector('i');
+//     wrapperEl.classList.toggle("active");
+//     if(iconEl.classList.contains('ri-share-line')){
+//         iconEl.classList.replace('ri-share-line','ri-close-line');
+//     }else {
+//         iconEl.classList.replace('ri-close-line','ri-share-line')
+//     }
+// };
+// btnEl.addEventListener('click',toggleOptions);
